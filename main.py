7a42d3c4888140a1cbf196ee98ad1c1ab2caf19e
Veginite <veginite@gmail.com>
2025-05-25 23:07:22 +0200

    moved the ClientSession to be a global in main.py to keep a constant connection up

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from typing import Final
 import os
 import aiosqlite
 from aiosqlite import Connection
+import aiohttp
 import discord
 from discord import Message, Interaction
 from discord.ext import commands
@@ -19,37 +20,43 @@ load_dotenv()
 DISCORD_TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
 DB_PASS: Final[str] = os.getenv("DB_PASSWORD")
 
+GGG_ACCESS_TOKEN: Final[str] = os.getenv("GGG_ACCESS_TOKEN")
+GGG_CLIENT_ID: Final[str] = os.getenv("GGG_CLIENT_ID")
+GGG_AUTHOR_CONTACT: Final[str] = os.getenv("GGG_AUTHOR_CONTACT")
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 dbc: Connection
+session: aiohttp.ClientSession
 
 
 # ------------- Commands -------------
 
-@bot.tree.command(name="addleague", description="Fetch a league from GGG API and add it to the database")
+@bot.tree.command(name="addleague", description="Fetch league data from GGG API and merge it to the database")
 async def add_league(interaction: Interaction, league_name: str):
 
-    await process_league(league_name, dbc)
+    await process_league(league_name, dbc, session)
 
-    await interaction.response.send_message(league_name)
+    await interaction.channel.send('processed league')
 
 
 @bot.tree.command(name="dbquery", description='You can run any database query with this')
 async def db_query(interaction: Interaction, query: str):
     if await run_db_query(dbc, query):
-        await interaction.response.send_message('Query executed')
+        await interaction.channel.send('Query executed')
     else:
-        await interaction.response.send_message('Query failed to execute')
+        await interaction.channel.send('Query failed to execute')
 
 
 @bot.tree.command(name='requestrank', description='Get your veteran roles with this!')
 async def request_rank(interaction: Interaction):
-    await interaction.response.send_message(f'Added rank to {interaction.user.mention}')
+    await interaction.channel.send(f'Added rank to {interaction.user.mention}')
 
 
 @bot.tree.command(name='testcode', description='Code playground')
 async def test_code(interaction: Interaction):
+    await interaction.channel.send('Testing code')
     return
 
 
@@ -74,10 +81,20 @@ async def on_ready() -> None:
     except Exception as e:
         print(e)
 
+    # instantiate a client session for HTTP requests from GGG's API
+
+    header = {
+        'Authorization': f'Bearer {GGG_ACCESS_TOKEN}',
+        'User-Agent': f'OAuth {GGG_CLIENT_ID}/1.0.0 (contact: {GGG_AUTHOR_CONTACT})'
+    }
+
+    global session
+    session = aiohttp.ClientSession(headers=header)
+
 
 @bot.event
 async def on_message(message: Message) -> None:
-    # Prevents a potential infinite message loop
+    # The bot should not react to message it posts itself or there will be an infinite message loop
     if message.author == bot.user:
         return
 
