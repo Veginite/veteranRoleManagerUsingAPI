22337c35b3fb32582b305396883b5c8705fdef06
Veginite <veginite@gmail.com>
2025-06-01 00:19:45 +0200

    Changed queries to utilize sqlite3 UPSERTS. Will change the processing flow to append all league entries before they're sent off to be queried to only make 3 executions per league, instead of once every 1-500 entries.

--- process_league.py
+++ process_league.py
@@ -7,17 +7,18 @@ from aiosqlite import Connection
 import aiohttp
 import json
 
-from db import run_db_query, table_entry_exists
+from db import run_db_query
 
 
 async def process_league(league_name: str, dbc: Connection, session) -> bool:
+    # FOR TESTING WITH LOCALLY SOURCED DATA
+    # with open('league_example.json') as f:
+        # league_data = json.load(f)
+
     # Initial API request, gives us information about how many ladder character entries the league has.
     # This allows us to precisely calculate the rest of the necessary API requests.
     # Grinding Gear Games has a hard limit of 500 entries per API request.
-
-    # league_data = await fetch_league_data(session, league_name, 500, 0)
-    with open('league_example.json') as f:
-        league_data = json.load(f)
+    league_data = await fetch_league_data(session, league_name, 500, 0)
 
     # If the league is brand new, add it to the database
     await process_league_entry(dbc, league_data["league"])
@@ -41,59 +42,62 @@ async def fetch_league_data(session: aiohttp.ClientSession, league_name: str, li
     url = f'https://api.pathofexile.com/league/{league_name}/ladder?limit={limit}&offset={offset}'
 
     async with session.get(url) as response:
-        return json.loads(await response.text()) if response.status == 200 else None
+        league_data = json.loads(await response.text())
+        if response.status == 200:
+            fetched_entries = len(league_data["ladder"]["entries"])
+            total_entries = league_data["ladder"]["total"]
+            print(f'{league_name}: Fetched entries: {offset+1}-{offset+min(limit, fetched_entries)+1}/{total_entries}.')
+            return league_data
+        else:
+            print(str(response.status))
+            return None
 
 
 async def process_ladder_entries(dbc, ladder_entries, league_name):
+    account_entries = []
+    character_entries = []
     for entry in ladder_entries:
         character = entry["character"]
 
         # New accounts need to be added before the characters because the foreign key is not allowed to be null
-        if not await table_entry_exists(dbc, "account", "username", entry["account"]["name"]):
-            params = {'username': entry["account"]["name"]}
+        account_entries.append({'username': entry["account"]["name"]})
 
-            query = f'INSERT INTO account (username) VALUES (:username);'
+        character_entries.append({
+            'id': character["id"],
+            'name': character["name"],
+            'rank': entry["rank"],
+            'class': character["class"],
+            'level': character["level"],
+            'experience': character["experience"],
+            'owner': entry["account"]["name"],
+            'league_name': league_name})
 
-            await run_db_query(dbc, query, params)
+    query = f'INSERT INTO account (username) VALUES (:username) ON CONFLICT(username) DO NOTHING;'
+    await run_db_query(dbc, query, account_entries)
 
-        # Update the character data if it does not already exist or insert a new entry if it's a new character
-        if await table_entry_exists(dbc, "character", "id", character["id"]):
-            params = {'name': character["name"],
-                      'rank': entry["rank"],
-                      'class': character["class"],
-                      'level': character["level"],
-                      'experience': character["experience"],
-                      'id': character["id"]}
+    # ------------------------
 
-            query = f'UPDATE character SET name=:name, rank=:rank, class=:class, level=:level, experience=:experience WHERE id=:id;'
+    subquery_owner = f'SELECT id FROM account WHERE username = :owner'  # FK surrogate key
+    subquery_league = f'SELECT id FROM league WHERE name = :league_name'  # FK surrogate key
 
-            await run_db_query(dbc, query, params)
+    # sqlite3 UPSERT implementation
+    query = (f'INSERT INTO character (id, name, rank, class, level, experience, owner, league) '
+             f'VALUES(:id, :name, :rank, :class, :level, :experience, ({subquery_owner}), ({subquery_league})) '
+             f'ON CONFLICT(id) DO '
+             f'UPDATE SET name=:name, rank=:rank, class=:class, level=:level, experience=:experience WHERE id=:id;')
+    await run_db_query(dbc, query, character_entries)
 
-        else:
-            params = {'id': character["id"],
-                      'name': character["name"],
-                      'rank': entry["rank"],
-                      'class': character["class"],
-                      'level': character["level"],
-                      'experience': character["experience"],
-                      'owner': entry["account"]["name"],
-                      'league_name': league_name}
-
-            subquery_owner = f'SELECT id FROM account WHERE username = :owner'  # FK surrogate key
-            subquery_league = f'SELECT id FROM league WHERE name = :league_name'  # FK surrogate key
-
-            query = (f'INSERT INTO character (id, name, rank, class, level, experience, owner, league) '
-                     f'VALUES(:id, :name, :rank, :class, :level, :experience, ({subquery_owner}), ({subquery_league}))')
-
-            await run_db_query(dbc, query, params)
+    # ------------------------
 
 
 async def process_league_entry(dbc, league_data):
-    if not await table_entry_exists(dbc, "league", "name", league_data["name"]):
-        params = {'league_name': league_data["name"],
-                  'start_date': league_data["startAt"],
-                  'end_date': league_data["endAt"]}
+    league_entry = {
+        'league_name': league_data["name"],
+        'start_date': league_data["startAt"],
+        'end_date': league_data["endAt"]}
 
-        query = f'INSERT INTO league (name, start_date, end_date) VALUES (:league_name, :start_date, :end_date)'
+    query = (f'INSERT INTO league (name, start_date, end_date) '
+             f'VALUES (:league_name, :start_date, :end_date) '
+             f'ON CONFLICT(name) DO UPDATE SET start_date=:start_date, end_date=:end_date;')
 
-        await run_db_query(dbc, query, params)
+    await run_db_query(dbc, query, league_entry)
