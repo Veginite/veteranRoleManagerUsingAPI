4976cbe3a8995427e427d9bd16a5dfe44785b3c7
Veginite <veginite@gmail.com>
2025-06-05 00:29:46 +0200

    added support for both single and multiple queries, alongside some minor changes

--- db.py
+++ db.py
@@ -9,10 +9,19 @@ from aiosqlite import Connection, Error
 async def run_db_query(dbc: Connection, query: str, params):
     cursor = await dbc.cursor()
     try:
-        await cursor.execute(query, params)
+        # If there's only one entry, use 'execute'
+        if type(params) is dict:
+            await cursor.execute(query, params)
+        # If there are multiple entries, use 'executemany'
+        elif type(params) is list:
+            await cursor.executemany(query, params)
+
         await dbc.commit()
         print(f"Query: '{query}' executed successfully.")
     except Error as error:
         print(f"Error: '{error}'" + " " + query)
-        return
-    return await cursor.fetchall()
+        await cursor.close()
+        return None
+    data = await cursor.fetchall()
+    await cursor.close()
+    return data
