c47a66c7256e581081b405ec22123095e56f8c2c
Veginite <veginite@gmail.com>
2025-06-05 00:33:08 +0200

    final fetch overhaul, now only makes a maximum of 3 round-trips to the database

--- process_league.py
+++ process_league.py
@@ -6,16 +6,19 @@
 from aiosqlite import Connection
 import aiohttp
 import json
+import time
 
 from db import run_db_query
 
 
 async def process_league(league_name: str, dbc: Connection, session) -> bool:
+    account_entries: list = []
+    character_entries: list = []
     # FOR TESTING WITH LOCALLY SOURCED DATA
-    # with open('league_example.json') as f:
+    # with open('league_example.json', encoding='utf-8') as f:
         # league_data = json.load(f)
 
-    # Initial API request, gives us information about how many ladder character entries the league has.
+    # The Initial API request gives us information about how many ladder character entries the league has.
     # This allows us to precisely calculate the rest of the necessary API requests.
     # Grinding Gear Games has a hard limit of 500 entries per API request.
     league_data = await fetch_league_data(session, league_name, 500, 0)
@@ -23,56 +26,34 @@ async def process_league(league_name: str, dbc: Connection, session) -> bool:
     # If the league is brand new, add it to the database
     await process_league_entry(dbc, league_data["league"])
 
-    await process_ladder_entries(dbc, league_data["ladder"]["entries"], league_data["league"]["name"])
+    await process_ladder_entries(dbc,
+                                 league_data["ladder"]["entries"],
+                                 account_entries,
+                                 character_entries,
+                                 league_data["league"]["name"])
+
     total_entries = int(league_data["ladder"]["total"])
     remaining_entries = total_entries - len(league_data["ladder"]["entries"])
     print("Remaining entries: " + str(remaining_entries))
 
     # Based on the number of ladder character entries left after the initial API request, fetch and process the rest
     while remaining_entries > 0:
+        time.sleep(0.05) # There is no guideline for rate-limiting, so I'll just go with 50 ms to be safe
         offset = total_entries - remaining_entries
         league_data = await fetch_league_data(session, league_name, min(remaining_entries, 500), offset)
-        await process_ladder_entries(dbc, league_data["ladder"]["entries"], league_data["league"]["name"])
+
+        await process_ladder_entries(dbc,
+                                     league_data["ladder"]["entries"],
+                                     account_entries,
+                                     character_entries,
+                                     league_data["league"]["name"])
+
         remaining_entries -= len(league_data["ladder"]["entries"])
 
-    return True
-
-
-async def fetch_league_data(session: aiohttp.ClientSession, league_name: str, limit: int, offset: int):
-    url = f'https://api.pathofexile.com/league/{league_name}/ladder?limit={limit}&offset={offset}'
-
-    async with session.get(url) as response:
-        league_data = json.loads(await response.text())
-        if response.status == 200:
-            fetched_entries = len(league_data["ladder"]["entries"])
-            total_entries = league_data["ladder"]["total"]
-            print(f'{league_name}: Fetched entries: {offset+1}-{offset+min(limit, fetched_entries)+1}/{total_entries}.')
-            return league_data
-        else:
-            print(str(response.status))
-            return None
-
-
-async def process_ladder_entries(dbc, ladder_entries, league_name):
-    account_entries = []
-    character_entries = []
-    for entry in ladder_entries:
-        character = entry["character"]
-
-        # New accounts need to be added before the characters because the foreign key is not allowed to be null
-        account_entries.append({'username': entry["account"]["name"]})
-
-        character_entries.append({
-            'id': character["id"],
-            'name': character["name"],
-            'rank': entry["rank"],
-            'class': character["class"],
-            'level': character["level"],
-            'experience': character["experience"],
-            'owner': entry["account"]["name"],
-            'league_name': league_name})
+    # ------------------------
 
     query = f'INSERT INTO account (username) VALUES (:username) ON CONFLICT(username) DO NOTHING;'
+
     await run_db_query(dbc, query, account_entries)
 
     # ------------------------
@@ -85,9 +66,45 @@ async def process_ladder_entries(dbc, ladder_entries, league_name):
              f'VALUES(:id, :name, :rank, :class, :level, :experience, ({subquery_owner}), ({subquery_league})) '
              f'ON CONFLICT(id) DO '
              f'UPDATE SET name=:name, rank=:rank, class=:class, level=:level, experience=:experience WHERE id=:id;')
+
     await run_db_query(dbc, query, character_entries)
 
     # ------------------------
+
+    return True
+
+
+async def fetch_league_data(session: aiohttp.ClientSession, league_name: str, limit: int, offset: int):
+    url = f'https://api.pathofexile.com/league/{league_name}/ladder?limit={limit}&offset={offset}'
+
+    async with session.get(url) as response:
+        league_data = json.loads(await response.text())
+        if response.status == 200:
+            fetched_entries = len(league_data["ladder"]["entries"])
+            total_entries = league_data["ladder"]["total"]
+            print(f'{league_name}: Fetched entries: {offset+1}-{offset+min(limit, fetched_entries)}/{total_entries}.')
+            return league_data
+        else:
+            print(str(response.status))
+            return None
+
+
+async def process_ladder_entries(dbc, ladder_entries, account_entries, character_entries, league_name):
+    for entry in ladder_entries:
+        character = entry["character"]
+
+        # New accounts need to be added before the characters because the foreign key is not allowed to be null
+        account_entries.append({'username': entry["account"]["name"]})
+
+        character_entries.append({
+            'id': character["id"],
+            'name': character["name"],
+            'rank': int(entry["rank"]),
+            'class': character["class"],
+            'level': int(character["level"]),
+            'experience': int(character["experience"]),
+            'owner': entry["account"]["name"],
+            'league_name': league_name})
 
 
 async def process_league_entry(dbc, league_data):
