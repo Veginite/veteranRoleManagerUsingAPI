1b559348ddc014fc04729f0f1b1f4765130a82a9
Veginite <veginite@gmail.com>
2025-06-08 02:35:49 +0200

    split the previous function into two

--- db.py
+++ db.py
@@ -6,22 +6,30 @@
 from aiosqlite import Connection, Error
 
 
-async def run_db_query(dbc: Connection, query: str, params):
+async def run_db_query(dbc: Connection, query: str, param: dict):
     cursor = await dbc.cursor()
     try:
-        # If there's only one entry, use 'execute'
-        if type(params) is dict:
-            await cursor.execute(query, params)
-        # If there are multiple entries, use 'executemany'
-        elif type(params) is list:
-            await cursor.executemany(query, params)
-
+        await cursor.execute(query, param)
         await dbc.commit()
         print(f"Query: '{query}' executed successfully.")
     except Error as error:
         print(f"Error: '{error}'" + " " + query)
         await cursor.close()
         return None
-    data = await cursor.fetchall()
+    rows = await cursor.fetchall()
     await cursor.close()
-    return data
+    return rows
+
+async def run_many_db_queries(dbc: Connection, query: str, params: list):
+    cursor = await dbc.cursor()
+    try:
+        await cursor.executemany(query, params)
+        await dbc.commit()
+        print(f"Query: '{query}' executed successfully.")
+    except Error as error:
+        print(f"Error: '{error}'" + " " + query)
+        await cursor.close()
+        return None
+    rows = await cursor.fetchall()
+    await cursor.close()
+    return rows
