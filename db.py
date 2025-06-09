7aabf93d189232ff53a7d126abdbea20f2eb9fed
Veginite <veginite@gmail.com>
2025-06-09 02:29:42 +0200

    moved commit lines

--- db.py
+++ db.py
@@ -3,6 +3,9 @@
 # Module status: FINISHED
 #########################################
 
+# Programmer note: both run_db_query and run_many_db_queries always returns a list of
+# at least one Tuple, or None if the query failed to execute.
+
 from aiosqlite import Connection, Error
 
 
@@ -10,29 +13,31 @@ async def run_db_query(dbc: Connection, query: str, param: dict):
     cursor = await dbc.cursor()
     try:
         await cursor.execute(query, param)
-        await dbc.commit()
         print(f"Query: '{query}' executed successfully.")
     except Error as error:
         print(f"Error: '{error}'" + " " + query)
         await cursor.close()
         return None
     rows = await cursor.fetchall()
+    await dbc.commit()
     await cursor.close()
     return rows
+
 
 async def run_many_db_queries(dbc: Connection, query: str, params: list):
     cursor = await dbc.cursor()
     try:
         await cursor.executemany(query, params)
-        await dbc.commit()
         print(f"Query: '{query}' executed successfully.")
     except Error as error:
         print(f"Error: '{error}'" + " " + query)
         await cursor.close()
         return None
     rows = await cursor.fetchall()
+    await dbc.commit()
     await cursor.close()
     return rows
+
 
 def get_generic_query_error_msg() -> str:
     return "Process aborted: Query failure. Ping Vegi."
