ebef50959bd69b1451f21ce50f66b3c3629f8ca4
Veginite <veginite@gmail.com>
2025-06-10 06:46:42 +0200

    added explicit return types

--- utils.py
+++ utils.py
@@ -8,9 +8,9 @@ import discord
 def get_host_mention() -> str:
     return f'<@{str(243795149291782146)}> '
 
-def query_was_unsuccessful(query_error: str):
+def query_was_unsuccessful(query_error: str) -> bool:
     return len(query_error) > 0
 
-async def purge_prior_roles(discord_user: discord.User, user_veteran_roles: list):
+async def purge_prior_roles(discord_user: discord.User, user_veteran_roles: list) -> None:
     for role in user_veteran_roles:
         await discord_user.remove_roles(discord_user.guild.get_role(role))
