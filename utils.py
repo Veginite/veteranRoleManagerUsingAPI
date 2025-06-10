bf1fc3b49aa90ee3c42739a8d94b44b06845dd75
Veginite <veginite@gmail.com>
2025-06-10 03:36:08 +0200

    moved a generic function here

--- utils.py
+++ utils.py
@@ -3,8 +3,14 @@
 # Module status: FINISHED
 #########################################
 
+import discord
+
 def get_host_mention() -> str:
     return f'<@{str(243795149291782146)}> '
 
 def query_was_unsuccessful(query_error: str):
     return len(query_error) > 0
+
+async def purge_prior_roles(discord_user: discord.User, user_veteran_roles: list):
+    for role in user_veteran_roles:
+        await discord_user.remove_roles(discord_user.guild.get_role(role))
